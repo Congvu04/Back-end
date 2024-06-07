a01a3ea22404d73b5ce5ef7738a4d01cf018e536
Sơn Elephants <vansonpham10112k1@gmail.com>
2024-06-07 21:27:45 +0700

    Initial commit

---
+++ hanhkiemUtehy/Migrations/20240531114909_DbInitit01.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hanhkiemUtehy.Migrations
+{
+    public partial class DbInitit01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
